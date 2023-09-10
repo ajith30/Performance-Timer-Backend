e0776f68286ebaeb7deeb954c54987e74d2a5385
ajith30 <ajithkumark30@gmail.com>
2023-09-10 15:36:26 +0530

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
 // Middleware for parse json 
 app.use(bodyParser.json());
+
+//implementing Cross-Origin Resource Sharing (CORS) in the server-side applications.
+app.use(cors());
 
 // Mount the route
 app.use("/", require("./routes/performanceRoute"));
